a31fb69fbb7ac936477b0f85ec4926de537f0207
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-10-20 15:26:33 -0300

    Add repo card component

---
+++ src/components/RepoCard/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import { Container, TopSide, RepoIcon, BotSide, StarIcon, ForkIcon } from './styles';
+
+declare interface Props {
+  username: string
+  reponame: string
+  description?: string
+  language?: string
+  stars: number
+  forks: number
+}
+
+const RepoCard: React.FC<Props> = (props) => {
+  const languageClass = props.language ? props.language.toLowerCase() : 'other'
+
+  return (
+    <Container>
+      <TopSide>
+        <header>
+          <RepoIcon />
+          <Link to={`/${props.username}/${props.reponame}`}>{props.reponame}</Link>
+        </header>
+        <p>{props.description}</p>
+      </TopSide>
+
+      <BotSide>
+        <ul>
+          <li>
+            <div className={`language ${languageClass}`} />
+            <span>{props.language}</span>
+          </li>
+          <li>
+            <StarIcon />
+            <span>{props.stars}</span>
+          </li>
+          <li>
+            <ForkIcon />
+            <span>{props.forks}</span>
+          </li>
+        </ul>
+      </BotSide>
+
+    </Container>
+  );
+}
+
+export default RepoCard;
