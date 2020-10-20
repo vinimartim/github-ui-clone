d9d66705374944423b2d319879ca7d47edbcdf9b
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-10-20 17:02:23 -0300

    Updating repo component

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, ForkIcon, LinkButton, GithubIcon } from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/vinimartim'}>
+          vinimartim
+        </Link>
+        
+        <span></span>
+
+        <Link className={'reponame'} to={'vinimartim/vendas-gv'}>
+          vendas-gv
+        </Link>
+      </Breadcrumb>
+
+      <p>Trocar depois</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/vinimartim/vendas-gv'}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
