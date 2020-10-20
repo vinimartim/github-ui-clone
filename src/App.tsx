e0b2d31af147a00c0c74e340e62483f05b675172
Vinicius Lisboa <lisboamvinicius@gmail.com>
2020-10-20 12:12:59 -0300

    Add global style to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import GlocalStyles from './styles/GlobalStyles'
 import Header from './components/Header'
 import Profile from './pages/Profile'
 import Repo from './pages/Repo'
@@ -17,6 +18,8 @@ function App() {
       </Routes>
 
       {/* <Footer /> */}
+
+      <GlocalStyles />
     </BrowserRouter>
   );
 }
